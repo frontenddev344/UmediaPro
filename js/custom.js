c3d64fd743880307a10f13cfcc22da4f950418de
nishant <nd@int.iqsay.com>
2024-08-22 12:32:54 +0530

    update changes

--- js/custom.js
+++ js/custom.js
@@ -33,9 +33,6 @@ gsap.to(".box",{
     var menu = document.querySelector(".menu-btn");
     var closedMenu = document.querySelector(".closed-menu");
     
-    
-    
-    
     var tl = gsap.timeline();
     
     tl.to("nav.site-nav", {
@@ -45,7 +42,7 @@ gsap.to(".box",{
     
     tl.from("nav.site-nav a", {
     x:100,
-    duration:0.7,
+    duration:0.4,
     stagger:0.2
     });
     
@@ -59,60 +56,6 @@ gsap.to(".box",{
     tl.reverse();
     });
     
-
-    // stacking card 
-    // gsap.registerPlugin(ScrollTrigger);
-
-    // const cards  = document.querySelectorAll('.card');
-    // // const header  = document.querySelector('.header');
-    // const animation = gsap.timeline()
-    // let cardHeight
-  
-  
-  
-    // function initCards(){
-    //   animation.clear()
-    //   cardHeight = cards[0].offsetHeight
-    //   console.log("initCards()", cardHeight)
-    //   cards.forEach((card, index) => {
-    //     if(index > 0){
-    //     //increment y value of each card by cardHeight
-    //       gsap.set(card, {y:index * cardHeight})
-    //     //animate each card back to 0 (for stacking)
-    //       animation.to(card, {y:0, duration:index*0.5, ease:"none"},0)
-  
-    //     }
-    //   })
-    // }
-  
-    // initCards()
-  
-    // ScrollTrigger.create({
-    //   trigger:".card-wrapper",
-    //   start:"top top",
-    //   pin:true,
-    //   end:()=>`+=${(cards.length * cardHeight)}`,
-    //   scrub:true,
-    //   animation:animation,
-    //   markers:true,
-    //   invalidateOnRefresh:true
-    // })
-  
-    // ScrollTrigger.addEventListener("refreshInit", initCards)
-  
-  
-    // Become a GSAP and ScrollTrigger Pro
-    // visit https://www.creativecodingclub.com/ 
-    // Unlock over 200 lessons and get new lessons weekly
-  
-    //automate scroll
-    /* gsap.delayedCall(1, ()=>{
-      gsap.to(window, {scrollTo:{y:"max", autoKill:true}, duration:15, repeat:1, yoyo:true, repeatDelay:1, ease:"none"})
-  
-    })
-     */
-
-
 // accordtion start 
 
 $(document).ready(function(){
@@ -122,14 +65,17 @@ $(document).ready(function(){
   });
 });
 
-let time = 90;
-let timeEl = document.querySelector(".time");
-let timer = setInterval(function(){
-  timeEl.innerHTML = time;
-  if(time <= 0){
-    clearInterval(timer);
-  } else {
-    time--;
-  }
-}, 500);
 
+let time = 0; 
+let timeEl = document.querySelector(".time");
+
+function updateTimer() {
+  timeEl.innerHTML = time.toString().padStart(2, "0"); 
+  if (time >= 90) {
+    time = 0; 
+  } else {
+    time++;
+  }
+}
+
+let timer = setInterval(updateTimer, 1000);
